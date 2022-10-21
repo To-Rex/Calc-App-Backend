6c0a0c8ce44b1cae7d3a2382504187667db3c031
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-21 23:20:35 +0500

    Calc Application Backend

--- go.mod
+++ go.mod
@@ -5,6 +5,18 @@ go 1.19
 require github.com/dgrijalva/jwt-go v3.2.0+incompatible
 
 require (
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.1 // indirect
+	github.com/xdg-go/stringprep v1.0.3 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+)
+
+require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
@@ -20,6 +32,7 @@ require (
 	github.com/obsc/async v0.0.0-20140730223756-a6e2df67745e // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
+	go.mongodb.org/mongo-driver v1.10.3
 	golang.org/x/crypto v0.1.0 // indirect
 	golang.org/x/net v0.1.0 // indirect
 	golang.org/x/sys v0.1.0 // indirect
--- main.go
+++ main.go
@@ -1,28 +1,144 @@
 package main
 
 import (
+	"context"
+	"net/http"
+	"time"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
-	"net/http"
-
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-const uri = "mongodb+srv://root:1234@cluster0.ik76ncs.mongodb.net/?retryWrites=true&w=majority"
+const uri = "mongodb+srv://CalcData:r5p3Gwuhn7ELIm3z@cluster0.vif5nkw.mongodb.net/?retryWrites=true&w=majority"
+//const uri = "mongodb+srv://root:1234@cluster0.ik76ncs.mongodb.net/?retryWrites=true&w=majority"
 
 type User struct{
 	email string `json:"email"`
 	password string `json:"password"`
+	verify string `json:"verify"`
+	times []string `json:"times"`
+	coments []string `json:"coments"`
+	timesWorks []string `json:"timesWorks"`
+	companets []string `json:"companets"`
+	token string `json:"token"`
+	
 }
 
 func main() {
 	r := gin.Default()
-	r.POST("/login", login)
 	r.POST("/register", register)
-	r.GET("/profile", profile)
+
 	r.Run(":8080")
 }
 
-//register user ansync
+func createToken(email string) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["email"] = email
+	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte("secret"))
+}
+func checkTokenMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+			return []byte("secret"), nil
+		})
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			c.Abort()
+			return
+		}
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			c.Set("email", claims["email"])
+		} else {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			c.Abort()
+			return
+		}
+	}
+}
+
+func connectDB() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		fmt.Println(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+	return client
+}
+
+func checkUser(client *mongo.Client, email string, password string) bool {
+	collection := client.Database("CalcData").Collection("users")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	var user User
+	err := collection.FindOne(ctx, bson.M{"email": email}).Decode(&user)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if user.password == password {
+		return true
+	}
+	return false
+}
+
+func insertUser(client *mongo.Client, user User) {
+	//if user available database return error
+
+	if user.email == "" || user.password == "" {
+		fmt.Println("Email or password is empty")
+		return
+	}
+
+	collection := client.Database("CalcData").Collection("users")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	_, err := collection.InsertOne(ctx, user)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("User inserted")
+}
+
+func register(c *gin.Context) {
+	var user User
+	err := c.BindJSON(&user)
+	if err != nil {
+		fmt.Println(err)
+	}
+	client := connectDB()
+	insertUser(client, user)
+	c.JSON(http.StatusOK, gin.H{"status": "ok"})
+}
+
+func getUser(client *mongo.Client, email string) User {
+	collection := client.Database("CalcData").Collection("users")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	var user User
+	err := collection.FindOne(ctx, bson.M{"email": email}).Decode(&user)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return user
+}
+
+
 
 
