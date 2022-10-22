a635d7937fecce087825e425f082c9c544a25965
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-22 21:17:25 +0500

    Calc Application Backend

--- main.go
+++ main.go
@@ -15,9 +15,9 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const uri = "mongodb+srv://CalcData:r5p3Gwuhn7ELIm3z@cluster0.vif5nkw.mongodb.net/?retryWrites=true&w=majority"
+//const uri = "mongodb+srv://CalcData:r5p3Gwuhn7ELIm3z@cluster0.vif5nkw.mongodb.net/?retryWrites=true&w=majority"
 
-//const uri = "mongodb+srv://root:1234@cluster0.ik76ncs.mongodb.net/?retryWrites=true&w=majority"
+const uri = "mongodb+srv://CalcData:r5p3Gwuhn7ELIm3z@cluster0.vif5nkw.mongodb.net/?retryWrites=true&w=majority"
 
 type User struct {
 	Email      string   `json:"email"`
@@ -42,6 +42,7 @@ func main() {
 	r.POST("verefyuser", verefyUser)
 	r.GET("getuser", getUser)
 	r.GET("getusers", getAllUsers)
+	r.POST("addtime", addTime)
 	r.Run(":8080")
 }
 func passwordHash(password string) string {
@@ -150,7 +151,6 @@ func cheskverefy(c *gin.Context) {
 
 func verefyUser(c *gin.Context) {
 	//post authorization bearer token user db update verefy to true and return token to client 
-
 	var user User
 	c.BindJSON(&user)
 	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
@@ -178,13 +178,17 @@ func verefyUser(c *gin.Context) {
 }
 
 func getUser(c *gin.Context) {
-	//get authorization bearer token return all user data
 	token := c.Request.Header.Get("Authorization")
-	if token == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "token is empty"})
+	token = token[7:len(token)]
+	claims := jwt.MapClaims{}
+	_, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte("secret"), nil
+	})
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "token is incorrect"})
 		return
 	}
-	
+	email := claims["email"].(string)
 	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
 	if err != nil {
 		fmt.Println(err)
@@ -193,15 +197,14 @@ func getUser(c *gin.Context) {
 	client.Connect(ctx)
 	defer client.Disconnect(ctx)
 	collection := client.Database("CalcData").Collection("users")
-	filter := bson.D{{Key: "token", Value: token}}
+	filter := bson.D{{Key: "email", Value: email}}
 	var result User
 	collection.FindOne(context.Background(), filter).Decode(&result)
-	if result.Token == token {
+	if result.Email == email {
 		c.JSON(http.StatusOK, result)
 		return
 	}
-	c.JSON(http.StatusBadRequest, gin.H{"error": "token is incorrect"})
-
+	c.JSON(http.StatusBadRequest, gin.H{"error": "email is incorrect"})
 }
 
 func getAllUsers(c *gin.Context) {
@@ -252,6 +255,35 @@ func getAllUsers(c *gin.Context) {
 		c.JSON(http.StatusOK, results)
 		return
 	}
+}
+
+func addTime(c *gin.Context) {
+	//post authorization bearer token user db update time
+	var user User
+	c.BindJSON(&user)
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		fmt.Println(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client.Connect(ctx)
+	defer client.Disconnect(ctx)
+	collection := client.Database("CalcData").Collection("users")
+	filter := bson.D{{Key: "email", Value: user.Email}}
+	var result User
+	collection.FindOne(context.Background(), filter).Decode(&result)
+	//add time in [{"time": "2020-12-12 12:12:12","coment","true"}] format
+	if result.Email == user.Email {
+		update := bson.D{
+			{Key: "$set", Value: bson.D{
+				{Key: "times", Value: user.Times},
+			}},
+		}
+		collection.UpdateOne(context.Background(), filter, update)
+		c.JSON(http.StatusOK, gin.H{"message": "time added"})
+		return
+	}
+	c.JSON(http.StatusBadRequest, gin.H{"error": "email is incorrect"})
 }
 
 func createToken(username string) string {
